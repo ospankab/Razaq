208d89a51c2c88f8aa278ce4d01367c6b786b9cf
Ospan Kabdollayev <68390859+ospankab@users.noreply.github.com>
2023-07-18 00:36:23 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,52 @@ function init() {
 
   myMap.geoObjects.add(myPlacemark);
 }
+
+const checkMenu = document.getElementById('check-menu');
+const headerInfo = document.querySelector('.header__info');
+
+checkMenu.addEventListener('change', function() {
+  if (window.innerWidth <= 670) {
+    if (this.checked) {
+      headerInfo.style.marginTop = '150px';
+    } else {
+      headerInfo.style.marginTop = '0';
+    }
+  }
+});
+
+
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 40,
+    mergeFit: true,
+    startPosition: 0,
+    items: 3,
+    nav: false,
+    dots: false,
+    autoWidth: false,
+    startPosition: 0,
+    responsive: {
+      0: {
+        items: 1
+      },
+      970: {
+        items: 2
+      },
+      1050: {
+        items: 3
+      }
+    }
+  });
+
+  $('.owl-prev').click(function() {
+    $('.owl-carousel').trigger('prev.owl.carousel');
+  });
+
+  $('.owl-next').click(function() {
+    $('.owl-carousel').trigger('next.owl.carousel');
+  });
+});
+
+
